953ee4eeb71b72bdb2db8177a662f35aebfc5c67
Yashwanth Pindi <pindiyashwanth@gmail.com>
2024-12-29 18:37:03 +0100

    feat: bfs updated

--- ImportantTopics/6_BFS.py
+++ ImportantTopics/6_BFS.py
@@ -1,4 +1,35 @@
+from collections import deque
 
+# BFS from given source s
+def bfs(adj, s):
+  
+    # Create a queue for BFS
+    q = deque()
+    
+    # Initially mark all the vertices as not visited
+    # When we push a vertex into the q, we mark it as 
+    # visited
+    visited = [False] * len(adj);
+    print(f'Visited Initially: {visited}')
+
+    # Mark the source node as visited and enqueue it
+    visited[s] = True
+    q.append(s)
+
+    # Iterate over the queue
+    while q:
+      
+        # Dequeue a vertex from queue and print it
+        curr = q.popleft()
+        print(curr, end=" Done one iteration ")
+        print()
+        # Get all adjacent vertices of the dequeued 
+        # vertex. If an adjacent has not been visited, 
+        # mark it visited and enqueue it
+        for x in adj[curr]:
+            if not visited[x]:
+                visited[x] = True
+                q.append(x)
 
 # Function to add an edge to the graph
 def add_edge(adj, u, v):
@@ -23,5 +54,5 @@ if __name__ == "__main__":
 
     # Perform BFS traversal starting from vertex 0
     print("BFS starting from 0: ")
-    print(adj)
-    # bfs(adj, 0)
+    print(f'the adjacency list initialised is: {adj}')
+    bfs(adj, 0)
