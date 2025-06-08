a96229e67f739a0a1dc2cbc7ec1148de1446a21a
Yashwanth Pindi <pindiyashwanth@gmail.com>
2025-06-08 14:21:37 +0200

    docs: adding output of chr and ord functions

--- LeetCode_AllPatterns/11_chr_ord.py
+++ LeetCode_AllPatterns/11_chr_ord.py
@@ -3,3 +3,8 @@ print(x)
 
 y = ord('a')
 print(y)
+
+# PS D:\Yashwanth\HTW_Berlin\Self_Learnings\Python> py .\LeetCode_AllPatterns\11_chr_ord.py
+# a
+# 97
+# PS D:\Yashwanth\HTW_Berlin\Self_Learnings\Python> 
