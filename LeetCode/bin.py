d942bc58527c0c8d5b84bdc33fa9dea77ee09092
Yashwanth Pindi <pindiyashwanth@gmail.com>
2026-09-24 15:39:22 +0100

    49. Group Anagrams

--- LeetCode/bin.py
+++ LeetCode/bin.py
@@ -1,10 +1,31 @@
-rows = [""]*5
-rows[2] += "Anu"
+# rows = [""]*5
+# rows[2] += "Anu"
 
-print(rows[2])
+# print(rows[2])
 
-visited = set()
-print(visited)
+# visited = set()
+# print(visited)
 
-visited.add(3)
-print(visited)
+# visited.add(3)
+# print(visited)
+
+# strs = ["eat", "tea"]
+# strs.pop()
+
+# print(strs)
+
+
+myDict1 = {"a": 1, "b": 2}
+myDict2 = {"b": 2, "a": 1}
+# if myDict1 == myDict2:
+#     print("Equal")
+# else:
+#     print("Not Equal")
+
+myListOfDicts = []
+myListOfDicts.append(myDict1)
+print(myListOfDicts)
+if myDict2 in myListOfDicts:
+    print("Found")
+else:
+    print("Not Found")
